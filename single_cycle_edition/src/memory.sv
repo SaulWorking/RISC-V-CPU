01f83f3be2571254fa28fae8fe9cf4938327f641
gavin <gavinhamon1234@gmail.com>
2026-03-07 16:44:04 -0500

    configured test_memory.py and its working but also made custom memory.sv i guess

---
+++ single_cycle_edition/src/memory.sv
@@ -0,0 +1,50 @@
+//memory.sv
+
+module memory #(
+	parameter WORDS = 64
+) (
+	input logic clk,
+	input logic [31:0] address,
+	input logic [31:0] write_data,
+	input logic write_enable,
+	input logic rst_n,
+
+	output logic [31:0] read_data
+);
+//following a guide:
+
+
+/*
+ the memory declaration below is byte addressed
+ no support for mis-aligned write nor reads.
+*/ 
+reg [31:0] mem [0:WORDS-1]; //memory array of 64 words, 32  bits.
+
+//num of bits needed to index memory
+localparam INDEX_BITS = $clog2(WORDS);
+
+always @ (posedge clk) begin
+	//reset logic
+	if(rst_n == 1'b0) begin
+		for(int i =0; i< WORDS; i++) begin
+			mem[i] <= 32'b0;
+		end
+	end
+	else if(write_enable) begin
+		//ensure address is aligned to word boundary
+		//else, ignore the write
+		if(address[1:0] == 2'b00) begin
+			//address[31:2] is the word index here..?
+			mem[address[INDEX_BITS+1:2]] <= write_data;
+		end
+	end
+end
+
+//read logic
+always_comb begin
+	//here, address[31:2] is the word index
+	read_data = mem[address[INDEX_BITS+1:2]];
+end
+
+endmodule
+
---
+++ single_cycle_edition/tb/memory/test_memory.py
@@ -0,0 +1,68 @@
+# test_memory.py
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
+
+
+@cocotb.test()
+async def memory_data_test(dut):
+    # start 10 nanosecond (ns) clock
+    cocotb.start_soon(Clock(dut.clk, 1, unit="ns").start())
+    await RisingEdge(dut.clk)
+
+    #reset + reset values
+    dut.rst_n.value = 0
+    dut.write_enable.value = 0
+    dut.address.value = 0
+    dut.write_data.value = 0
+
+    await RisingEdge(dut.clk)
+    dut.rst_n.value = 1
+    await RisingEdge(dut.clk)
+
+    # everything 0 after reset
+    for address in range(dut.WORDS.value):
+        dut.address.value = address
+        await Timer(1, units="ns")
+        assert dut.read_data.value == "00000000000000000000000000000000"
+
+    # test: write and read back random data
+    test_data = [
+            (0, 0xDEADBEEF), 
+            (4,0xCAFEBABE),
+            (8,0x12345678),
+            (12,0xA5a5a5a5)
+    ]
+
+    for address, data in test_data:
+        #write data to memory
+        dut.address.value = address
+        dut.write_data.value = data
+        dut.write_enable.value = 1
+        await RisingEdge(dut.clk)
+
+        #disable write after one cycle
+        dut.write_enable.value = 0
+        await RisingEdge(dut.clk)
+
+        #verify the write reading back
+        dut.address.value =address
+        await RisingEdge(dut.clk)
+        assert dut.read_data.value == data
+
+    
+    #test: writing to multiple addresses then reading them
+    for i in range(40,4):
+        dut.address.value = i
+        dut.write_data.value = i+100
+        dut.write_enable.value = 1
+        await RisingEdge(dut.clk)
+
+    #disable write, then read back values
+    dut.write_enable.value = 0    
+    for i in range(40,4):
+        dut.address.value = i
+        await RisingEdge(dut.clk)
+        expected_value = i + 100
+        assert dut.read_data.value == expected_value
