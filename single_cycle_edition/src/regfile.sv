b7a7d07fd900ccfcb27be7fa78f0290da26cdad9
gavin <gavinhamon1234@gmail.com>
2026-03-11 17:09:53 -0400

    dont know what im doing (writing code from github repo)

---
+++ single_cycle_edition/src/regfile.sv
@@ -0,0 +1,43 @@
+// regfile.sv
+
+module regfile(
+	// basic signals
+	input logic clk,
+	input logic rst_n,
+
+	//readers
+	input logic [4:0] address1,
+	input logic [4:0] address2,
+	output logic [31:0] read_data1,
+	output logic [31:0] read_data2,
+
+	//writers
+	input logic write_enable,
+	input logic [31:0] write_data,
+	input logic [4:0] address3
+);
+
+
+// 32 bit register addresed with 5 bits
+reg [31:0] registers [0:31];
+
+always @(posedge clk) begin
+	//reset support & init to 0
+	if(rst_n == 1'b0) begin
+		for(int i =0; i<32; i++) begin
+			registers[i] <= 32'b0
+		end
+	end
+
+	else if (write_enable == 1'b1 && address3 != 0) begin
+		registers[address3] <= write_data;
+	end
+end
+
+always_comb begin : readLogic
+	read_data1 = registers[address1];
+	read_data2 = registers[address2];
+end
+
+endmodule
+
