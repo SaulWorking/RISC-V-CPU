c204fccb76a3a77a9b7a6942e1653ea0a317c9c6
sohyh <gavinhamon1234@gmail.com>
2026-04-10 10:50:03 -0400

    wait for always _comb before next instrctuction

--- single_cycle_edition/tb/signext/test_signext.py
+++ single_cycle_edition/tb/signext/test_signext.py
@@ -1,5 +1,5 @@
 import cocotb
-from cocotb.clock import
+from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer
 import random
 import numpy as np
@@ -8,7 +8,7 @@ import numpy as np
 async def random_write_read_test(dut):
     # TEST POSITIVE IMM = 123 WITH SOURCE = 0
     imm = 0b000001111011
-    imm << = 13
+    imm <<= 13
     source = 0b00
 
     # 25 bits sent to signed extend contains data before that will be ignored(rd, f3,...)
@@ -16,8 +16,8 @@ async def random_write_read_test(dut):
     random_junk = 0b000000000000_1010101010101
     raw_data = random_junk | imm
     await Timer(1, unit="ns")
-    dut_raw_src.value = raw_data
-    dut.imm_source = source
+    dut.raw_src.value = raw_data
+    dut.imm_source.value = source
     await Timer(1, unit="ns") # let it propagate?
     assert dut.immediate.value == "00000000000000000000000001111011"
     assert int(dut.immediate.value) == 123
@@ -32,7 +32,8 @@ async def random_write_read_test(dut):
     raw_data = random_junk | imm
     await Timer(1, unit="ns")
     dut.raw_src.value = raw_data
-    dut.imm_source = source
-    assert dut.immediate.value == "111111111111111111111111111010110"
+    dut.imm_source.value = source
+    await Timer(1, unit="ns")
+    assert dut.immediate.value == "11111111111111111111111111010110"
     #python interprets int as unit. sub 1<,32 as int to get corresponding negative value
     assert int(dut.immediate.value) - (1 << 32) == -42
