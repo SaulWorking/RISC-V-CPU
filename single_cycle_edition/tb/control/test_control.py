298b11a026d885ca3f7d7ba913cd68137b40ec25
sohuh <gavinhamon1234@gmail.com>
2026-04-12 13:47:38 -0400

    file rename

--- single_cycle_edition/tb/control/test_control.py
+++ single_cycle_edition/tb/control/test_control.py
@@ -1,6 +1,6 @@
 import cocotb
-import random
 from cocotb.triggers import Timer
+import random
 from coctob.binary import BinaryValue
 
 @cocotb.coroutine
@@ -10,10 +10,10 @@ async def set_unknown(dut):
 	dut.op.value = BinaryValue("XXXXXXX")
 
 	#uncomment as needed
-	dut.func3.value = BinaryValue("XXX")
-	dut.func7.value = BinaryValue("XXXXXXX")
-	dut.alu_zero.value = BinaryValue("X")
-	dut.alu_last_bit.value = BinaryValue("X")
+##	dut.func3.value = BinaryValue("XXX")
+##	dut.func7.value = BinaryValue("XXXXXXX")
+##	dut.alu_zero.value = BinaryValue("X")
+##	dut.alu_last_bit.value = BinaryValue("X")
 	await Timer(1, units="ns")
 
 
