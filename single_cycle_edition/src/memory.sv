56419ace3bbd35e13bf5ab10aa3e75a7f81ba8f4
sohuh <gavinhamon1234@gmail.com>
2026-09-06 20:59:53 -0400

    trying to 'relearn'

--- single_cycle_edition/src/memory.sv
+++ single_cycle_edition/src/memory.sv
@@ -1,58 +1,60 @@
-//memory.sv
-
-module memory #(
-	parameter WORDS = 64,
-	parameter mem_init = ""
+module instruction_memory #(
+    parameter WORDS = 64
 ) (
-	input logic clk,
-	input logic [31:0] address,
-	input logic [31:0] write_data,
-	input logic write_enable,
-	input logic rst_n,
+    // little endian notation
+    input logic clk, // main clock of cpu
+    input logic [31:0] address, // data final destination
+    input logic [31:0] write_data, // start data
+    input logic write_enable, // write check
+    input logic rst_n, // reset check
 
-	output logic [31:0] read_data
+    output logic [31:0] read_data // end data
 );
 
-
 /*
- the memory declaration below is byte addressed
- no support for mis-aligned write nor reads.
-*/ 
-reg [31:0] mem [0:WORDS-1]; //memory array of 64 words, 32  bits.
-
-initial begin
-	$readmemh(mem_init, mem);
-end
-
-always @ (posedge clk) begin
-	//reset logic
-	if(rst_n == 1'b0) begin
-		for(int i =0; i< WORDS; i++) begin
-			mem[i] <= 32'b0;
-		end
-	end
-	else if(write_enable == 1'b1) begin
-		//ensure address is aligned to word boundary ( last 2 bits are
-		//useless i guess)
-		//else, ignore the write
-		
-		if(address[1:0] == 2'b00) begin
-			//force truncate (<= is non-blocking [sequential])
-
-			/* verilator lint_off WIDTHTRUNC */ 
-			mem[address[31:2]] <= write_data;
-		end
-	end
-end
-
-/**
-* runs when input changes (write_enable changes)
+* memory is byte addressed for our particular cases
+*   - each memory address is 8 bits of data
+* currently, there is no solution for mis-aligned write + read
 */
+
+
+// WORD is 4 bytes word
+
+reg [31:0] mem [0:WORDS-1] // array of WORDS, of size WORD 
+
+
+//sequential logic, use non-blocking <=
+always @(posedge clk) begin
+
+    if (rst_n == 1'b0) begin
+
+        for(int i = 0; i < WORDS; i++) begin
+            mem[i] <= 32'b0;
+        end
+
+    end
+    else if (write_enable) begin
+
+        // align address to word boundary
+        // last 2 bits of word means it is divisible by 4
+
+        if (address[1:0] == 2'b00) begin
+            //we index memory by [31:2]
+            mem[address[31:2]] <= write_data;
+        end
+    end
+end
+
+
+//mem[address[31:2]] added to sensitivity list
+
+//@ t=0, read_data = x
+
 always_comb begin
-	//force truncate	
-	/* verilator lint_off WIDTHTRUNC */
-	read_data = mem[address[31:2]];
+    read_data = mem[address[31:2]];
 end
 
 endmodule
 
+
+endmodule
